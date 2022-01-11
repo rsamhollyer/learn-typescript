cbe54c8b6a00559604bc23b26371e12e2329bf56
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-11 15:00:49 -0600

    update todo form handlers

--- typescript-react/src/components/NewTodo.tsx
+++ typescript-react/src/components/NewTodo.tsx
@@ -1,12 +1,20 @@
+import { useRef } from 'react';
+
 const NewTodo = () => {
+  const todoTextRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+    const enteredText = todoTextRef.current?.value;
+    if (enteredText?.trim()?.length === 0) {
+      throw Error('You must enter a todo!');
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="todo">New Todo</label>
-      <input id="todo" type="text" />
+      <input id="todo" type="text" ref={todoTextRef} />
       <button type="submit">Add Todo</button>
     </form>
   );
