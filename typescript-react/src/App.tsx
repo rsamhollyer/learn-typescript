579429ed0ec69ec0882fa63fa60c5e1b5c348fc8
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-16 03:14:41 -0600

    add todo usestate and type

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
-  const setNewTodo = (todoText: string) => {};
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const setNewTodo = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos(prev => prev.concat(newTodo));
+  };
+
   return (
     <div className="App">
       <NewTodo onAddTodo={setNewTodo} />
