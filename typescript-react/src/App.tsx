2c53902b76abd19e904387f979c58ae92338a1b8
Sam Hollyer <rsamhollyer@gmail.com>
2022-01-16 03:10:21 -0600

    add todo add handler

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
   const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
+  const setNewTodo = (todoText: string) => {};
   return (
     <div className="App">
+      <NewTodo onAddTodo={setNewTodo} />
       <Todos todos={todos} />
     </div>
   );
--- typescript-react/src/components/NewTodo.tsx
+++ typescript-react/src/components/NewTodo.tsx
@@ -1,14 +1,19 @@
 import { useRef } from 'react';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = ({
+  onAddTodo,
+}) => {
   const todoTextRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+
     const enteredText = todoTextRef.current?.value;
+
     if (enteredText?.trim()?.length === 0) {
       throw Error('You must enter a todo!');
     }
+    if (enteredText) onAddTodo(enteredText);
   };
 
   return (
